fdbb1f0b1d38a2822f102aa1b4954bbdbaa80fa4
ozzkan-burak <brk.ozzkan@gmail.com>
2023-03-30 22:48:50 +0300

    episode4

--- app.js
+++ app.js
@@ -8,27 +8,40 @@ const booksArray = [
     author: "ABC",
     id: 1,
     isActive: false,
-    price: 12.77
+    price: 12.77,
+    user: {
+      id: 1000,
+      name: 'Uras',
+      surname: 'Özkan'
+      }
   },
   {
     title: "Kitap 2",
     author: "DEF",
     id: 2,
     isActive: true,
-    price: 16.20
+    price: 16.20,
+    user :{
+      id: 100,
+      name: 'Burak',
+      surname: 'Özkan'
+    }
   },
 ]
 
 const usersArray = [
   {
+    id: 100,
     name: 'Burak',
     surname: 'Özkan'
   },
   {
+    id: 1000,
     name: 'Uras',
     surname: 'Özkan'
   },
   {
+    id: 101,
     name: 'Buse',
     surname: 'Özkan'
   }
@@ -41,21 +54,27 @@ const typeDefs = `#graphql
     id:Int!
     isActive: Boolean!
     price: Float!
+    user: User!
   }
 
   type User {
     name: String!
     surname: String!
+    id: Int!
   }
 
   type Query {
-    books: [Book]
+    books: [Book]!
     users: [User]
+    user(id: Int): User!
   }
 `;
 
 const resolvers = {
   Query: {
+    user:(_, args) => {
+      console.log(args)
+    },
     books: ()=> {
       return booksArray;
     },
